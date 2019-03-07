1c5779c1e9f83b725512f8a4b8178bc6ac4fdc7d
czw <chenzhiwei.ah@163.com>
2019-03-08 02:39:29 +0800

    添加注释说明

--- scikit-learn_tutorial/AdaboostClassifier.py
+++ scikit-learn_tutorial/AdaboostClassifier.py
@@ -1,3 +1,16 @@
+# -*- utf-8 -*-
+'''
+@purpose: 1. 测试优化器在读入Dataframe格式数据时，优化器将其会不会转换为np.array()格式数据。
+           2.clf.predict_proba() 结果为概率数组，数组标签顺序是什么样，是否同标签相对应的“数字label” 排序一致。如：1, 2, 3, ...
+@result: 1.优化器fit时先判断是否为np.array(),否的话，转为np.array() 后进行fit.
+        2.结果数组标签顺序同“数字标签”顺序一致。如：
+        array：
+                    label_1     label_2
+                0   0.45        0.55
+                1   0.6         0.4
+@method: debug || step in || read source code!
+'''
+
 import pandas as pd
 import numpy as np
 import os
